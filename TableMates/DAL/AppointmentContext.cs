996691f347278355cc5c9fe2d0f7f789f4f76293
suiname <jtham@sbcglobal.net>
2016-01-27 12:02:48 -0600

    Added DB context, finalized models

---
+++ TableMates/DAL/AppointmentContext.cs
@@ -0,0 +1,22 @@
+﻿using TableMates.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TableMates.DAL
+{
+    public class TablemateContext : DbContext
+    {
+
+        public TablemateContext() : base("TablemateContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ TableMates/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TableMates.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string ID { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public virtual ICollection<Appointment> Appointments { get; set; }
+
+        
+    }
+}
